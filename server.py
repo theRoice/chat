2bee325231c18bb67f91395e88fe8c22968217e2
bard <dev@leachman.io>
2026-02-03 19:37:41 -0800

    implementation of 'thepythoncode' server code

---
+++ server.py
@@ -0,0 +1,51 @@
+import socket
+
+from threading import Thread
+
+# initial python server code taken from: https://www.thepythoncode.com/article/make-a-chat-room-application-in-python
+
+# SETUP
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5002
+separator_token = "<SEP>"
+
+# Initialization
+client_sockets = set()
+s = socket.socket()
+
+s.setsocketopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((SERVER_HOST, SERVER_PORT))
+
+s.listen(5)
+
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+def listen_for_client(cs):
+    while True:
+        try:
+            message = cs.recv(1024).decode()
+        except Exception as e:
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            msg = message.replace(separator_token, ": ")
+
+        for client_socket in client_sockets:
+            client_socket.send(msg.encode())
+
+
+while True:
+    client_socket, client_address = s.accept()
+
+    print(f"[+] {client_address} connected.")
+    client_sockets.add(client_socket)
+    
+    t = Thread(target=listen_for_client, args=(client_socket,))
+
+    t.daemon = True
+    t.start()
+
+for cs in client_sockets:
+    cs.close()
+
+s.close()
